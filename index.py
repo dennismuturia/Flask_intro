6f193797096ff4d35cee2c320d3d2b692bf2f923
dennismuturia <dmuturia2922@gmail.com>
2017-12-10 18:35:22 +0300

    Learned on the get and post methods in flask

--- index.py
+++ index.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+#I will be dealing with HTTP requests both Get and Post
 
 app = Flask(__name__)
 
+#Let us implement the get and post method
 @app.route('/')
 def index():
-    return 'This is the home page'
+    return 'Method used is %s' % request.method
+
+@app.route('/bacon', methods = ['GET', 'POST'])
+def bacon():
+    if request.method == 'GET':
+        return 'You are using the GET'
+    else:
+        return 'You are probably using POST'
 
 @app.route('/about')
 def about():
     return '<h2>Hey this is mt site</h2>'
+
+@app.route('/profile/<username>')
+def profile(username):
+    return 'Hey there %s' % username
 
 if __name__ =="__main__":
     app.run(debug=True)
