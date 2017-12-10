a955a9ed535474b4c07d2f5c079e1d185f8e3514
dennismuturia <dmuturia2922@gmail.com>
2017-12-10 18:04:28 +0300

    Able to create routes in flask

--- index.py
+++ index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'This is the home page'
 
+@app.route('/about')
+def about():
+    return '<h2>Hey this is mt site</h2>'
+
 if __name__ =="__main__":
     app.run(debug=True)
