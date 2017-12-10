8dd2c168006f8cdde140e6caf04c908d76c6d82b
dennismuturia <dmuturia2922@gmail.com>
2017-12-10 18:48:48 +0300

    Created two directories. That will be used to store our templates and static files

--- index.py
+++ index.py
@@ -1,27 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 #I will be dealing with HTTP requests both Get and Post
 
 app = Flask(__name__)
 
-#Let us implement the get and post method
+#Let us implement the get and post method and using of templates
 @app.route('/')
 def index():
     return 'Method used is %s' % request.method
 
-@app.route('/bacon', methods = ['GET', 'POST'])
-def bacon():
-    if request.method == 'GET':
-        return 'You are using the GET'
-    else:
-        return 'You are probably using POST'
 
-@app.route('/about')
-def about():
-    return '<h2>Hey this is mt site</h2>'
-
-@app.route('/profile/<username>')
-def profile(username):
-    return 'Hey there %s' % username
 
 if __name__ =="__main__":
     app.run(debug=True)
