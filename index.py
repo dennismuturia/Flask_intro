e96d5fc9a5ddc7803a1ce447a01225e5d55abb83
dennismuturia <dmuturia2922@gmail.com>
2017-12-10 17:47:19 +0300

    Starting and configuring the server todeveloper mode

--- index.py
+++ index.py
@@ -3,5 +3,8 @@ from flask import flask
 app = Flask(__name__)
 
 @app.route('/')
-def index:
-    print("This is the home page")
+def index():
+    return 'This is the home page'
+
+if __name__ == "__main__":
+    @app.run(debug=True)
