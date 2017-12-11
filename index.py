cb260e82d63d8a792ec918507c10fc4eb9f16000
dennismuturia <dmuturia2922@gmail.com>
2017-12-11 13:45:20 +0300

    Integrating the about page that will hold my own information as a programmer

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+#I will install flask assets
+from flask_assets import Bundle, Environment
 #I will be dealing with HTTP requests both Get and Post
 
 app = Flask(__name__)
@@ -6,9 +8,14 @@ app = Flask(__name__)
 #Let us implement the get and post method and using of templates
 #Lets incorporate another url in this index
 @app.route('/')
-@app.route('/<name>')
-def index(name = None):
-    return render_template("index.html", name=name)
+def index():
+    return render_template("index.html")
 #Later on today I will try and incorporate bootstrap and route through the navigation bar
+#Implementing flask assets in the project
+
+#This will be the about page
+@app.route('/about')
+def about():
+    return render_template("about.html")
 if __name__ =="__main__":
     app.run(debug=True)
