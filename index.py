bc00d8d7750c24e1cae878fdefe8405b9040d7eb
dennismuturia <dmuturia2922@gmail.com>
2017-12-11 16:28:33 +0300

    Inserted the navigation bar using jinja flask bootstrap.

--- index.py
+++ index.py
@@ -1,18 +1,16 @@
 from flask import Flask, request, render_template
-from flask.ext.bootstrap import Bootstrap
-#I will install flask assets
-#I will be dealing with HTTP requests both Get and Post
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
 
 #Let us implement the get and post method and using of templates
-#Lets incorporate another url in this index
-@app.route('/index', methods=['GET', 'POST'])
+
+@app.route('/')
 def index():
     return render_template("index.html")
 #Later on today I will try and incorporate bootstrap and route through the navigation bar
-#Implementing flask assets in the project
 
 #This will be the about page
 @app.route('/about')
